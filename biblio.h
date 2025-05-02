14c4ecf8978caa78b6075379dece5afd313a1893
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-05-02 10:13:52 +0200

    Update biblio.h

--- biblio.h
+++ biblio.h
@@ -8,10 +8,10 @@
 #define TAILLECHAINE 40
 
 // On définit les emojis utilisés pour la carte
-/*"\xE2\x97\xBB"*/#define EMOJI_NEIGE    " "
-/*"\xF0\x9F\xAA\xA8"*/#define EMOJI_PIERRE   "O"
-/*"\xF0\x9F\xAA\xA8"*/#define EMOJI_DRAPEAU  "F"
-/*"\xF0\x9F\x8C\xB8"*/#define EMOJI_SAPIN    "A"
+#define EMOJI_NEIGE           "\xE2\x97\xBB\xEF\xB8\x8F"
+#define EMOJI_PIERRE          "\xF0\x9F\xAA\xA8"
+#define EMOJI_DRAPEAU         "\xF0\x9F\x9A\xA9"
+#define EMOJI_SAPIN           "\xF0\x9F\x8C\xB2"
 #define EMOJI_PINGOUIN        "\xF0\x9F\x90\xA7"
 #define EMOJI_BONHOMMENEIGE   "\xE2\x9B\x84"
 #define EMOJI_OURS            "\xF0\x9F\x90\xBB\xE2\x80\x8D\xE2\x98\x84"
