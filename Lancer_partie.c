912431e018a7207c59337958363f265d4774ca9a
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-05-02 11:06:51 +0200

    Create Lancer_partie.c

---
+++ Lancer_partie.c
@@ -0,0 +1,31 @@
+void Partie (){
+    srand(time(NULL));
+    int taillecarte; //Variable pour la taille de la carte
+
+    // On génère une taille de carte aléatoire entre 15 et 25
+    taillecarte=rand()%11+15;
+
+    int **carte=(int**)malloc(taillecarte*sizeof(int*)); //Allocation dynamique de la mémoire pour la carte
+    
+    creer_carte(carte, taillecarte);
+    creer_chemin(carte, taillecarte);
+    afficher_carte(carte, taillecarte);
+    placement_de_defenseur(Case carte[]);
+
+    while(couronne){
+        sleep(1);
+        deplacement_attaquant();
+        generer_attaquant();
+        afficher_carte(carte, taillecarte);
+        attaque_defenseurs();
+        placement_de_defenseur(Case carte[]);
+        Amelioration();
+    }
+    
+    //On libère la mémoire allouée pour la carte
+    for (int i=0; i<taillecarte; i++) {
+    free(carte[i]);
+    }
+    free(carte);
+    return 0;
+}
