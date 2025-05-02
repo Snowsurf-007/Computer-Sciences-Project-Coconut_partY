9bc84f8dd064c9d056a78f95951c25f0c2067856
M4x0u77 <max19.monteil@gmail.com>
2025-05-02 16:30:32 +0200

    Add files via upload

---
+++ Deplacement_Attaquant.c
@@ -0,0 +1,33 @@
+
+
+	void Deplacement_Attaquant(int carte[][],int taillecarte) {
+	
+		int colonneC=0;//position de la colonne de la couronne
+		
+		for(int j=0; j<taillecarte; j++) {
+			
+			if(carte[taille-1][j]==12) {
+				
+				positionC=j;
+			}
+		} 
+		
+			
+			
+		do {
+		
+			if(carte[taillecarte-2][j]==13 || carte[taillecarte-2][j]==14 || carte[taillecarte-2][j]==15) {
+			
+				
+			}
+			
+			
+			
+		}while(carte[i][j]!=12);
+		
+		
+		
+		
+		
+		
+	}
