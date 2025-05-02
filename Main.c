deba864b36094422974734ef52aeae9eb08b3461
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-05-02 11:08:49 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -9,10 +9,13 @@ int main (){
     taillecarte=rand()%11+15;
 
     int **carte=(int**)malloc(taillecarte*sizeof(int*)); //Allocation dynamique de la mémoire pour la carte
-    
     creer_carte(carte, taillecarte);
     creer_chemin(carte, taillecarte);
     afficher_carte(carte, taillecarte);
+
+    while(){
+        
+    }
 
     //On libère la mémoire allouée pour la carte
     for (int i=0; i<taillecarte; i++) {
