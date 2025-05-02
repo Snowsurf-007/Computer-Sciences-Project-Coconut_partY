8186b90dfb0e3084d626277fee630f1195fe06e1
Raphyduff <raphael.dufour@etu.cyu.fr>
2025-05-02 10:51:39 +0200

    Create génération_des_crabes.c

---
+++ génération_des_crabes.c
@@ -0,0 +1,23 @@
+int generer_ennemi(Case carte[], Attaquant liste_attaquant[], int nb_attaquant){
+  
+  //une chance sur 2 de générer un nouvel ennemi, renvoie le nombre d'attaquant actuel
+  
+  if(rand()%4){
+    nb_attaquant += 1;
+    Attaquant nouv_ennemi;
+    int numero = -1; //permet de choisir quel attaquant faire apparaitre
+    numero = rand() % 3;
+    
+    if(numero == 0){
+      constructeur_SkieurFrenetique(nouv_ennemi);
+    }
+    else if(numero == 1){
+      constructeur_SnowboarderAcrobate(nouv_ennemi);
+    }
+    else if(numero == 2){
+      constructeur_LugisteBarjo(nouv_ennemi);
+    }
+    liste_attaquant[nb_attaquant] = nouv_ennemi;
+  }
+  return nb_attaquant;
+}
