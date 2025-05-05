6712225354c5d9a7db6c48ce3009e113a1429c39
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-05-05 15:26:18 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,22 +1,7 @@
 #include "Biblio.h"
 
 int main (){
-    srand(time(NULL));
-    int taillecarte; //Variable pour la taille de la carte
+    MenuDemarrage();
 
-    // On génère une taille de carte aléatoire entre 35 et 50
-    taillecarte=rand()%16+35;
-
-    int **carte=(int**)malloc(taillecarte*sizeof(int*)); //Allocation dynamique de la mémoire pour la carte
-    
-    creer_carte(carte, taillecarte);
-    creer_chemin(carte, taillecarte);
-    afficher_carte(carte, taillecarte);
-
-    //On libère la mémoire allouée pour la carte
-    for (int i=0; i<taillecarte; i++) {
-    free(carte[i]);
-    }
-    free(carte);
     return 0;
 }
