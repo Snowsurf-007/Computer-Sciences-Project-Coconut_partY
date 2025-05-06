47e594093322ff70ae7376ac91d79883c43ff84a
M4x0u77 <max19.monteil@gmail.com>
2025-05-06 11:26:11 +0200

    Create interfaceUtilisateur.h

---
+++ interfaceUtilisateur.h
@@ -0,0 +1,10 @@
+#ifndef_
+
+menuDemarrage();
+afficher_carte(int **carte, int taillecarte);
+creer_carte(int **carte, int taillecarte);
+creer_chemin(int **carte, int taillecarte);
+placement_de_defenseur(int carte[][], int taillecarte);
+lancerpartie ();
+
+#endif
