95b26bff08d4d71eacd111e359f997599a267953
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-05-06 11:20:53 +0200

    Update Fonction.h

--- Biblio.h
+++ Biblio.h
@@ -18,3 +18,84 @@
 #define EMOJI_COURONNE        "\xF0\x9F\x91\x91"
 // On définit les constantes
 #define TAILLECHAINE 16
+
+typedef struct{
+    char emoji[TAILLECHAINE]; //code UTF8 emoji du defenseur
+    int portée;
+    int dégats;
+    float vitessetir;
+    int prix;
+    int coordx;
+    int coordy;
+}Defenseur;
+
+typedef struct{
+    char emoji[TAILLECHAINE]; //code UTF8 emoji de l'attaquant
+    int vie;
+    float esquive;
+    int gain;
+}Attaquant;
+
+Defenseur constructeur_PinguPatrouilleur(Defenseur a){
+    strcpy(a.emoji, EMOJI_PINGOUIN);
+    a.portée=5;
+    a.dégats=30;
+    a.vitessetir=0.5;
+    a.prix=100;
+    return a;
+}
+
+Defenseur constructeur_FloconPerceCiel(Defenseur a){
+    strcpy(a.emoji, EMOJI_BONHOMMENEIGE);
+    a.portée=10;
+    a.dégats=300;
+    a.vitessetir=2;
+    a.prix=200;
+    return a;
+}
+
+Defenseur constructeur_GardePolaire(Defenseur a){
+    strcpy(a.emoji, EMOJI_OURS);
+    a.portée=2;
+    a.dégats=70;
+    a.vitessetir=1;
+    a.prix=150;
+    return a;
+}
+
+Attaquant constructeur_SkieurFrenetique(Attaquant a){ //attaquant rapide et faible, petit taux d'esquive 
+    strcpy(a.emoji, EMOJI_SKIEUR);
+    a.vie=250;
+    a.esquive=0.15;
+    a.gain=20;
+    return a;
+}
+
+Attaquant constructeur_SnowboarderAcrobate(Attaquant a){ //attaquant vitesse moyenne, vie moyenne mais bonne esquive
+    strcpy(a.emoji, EMOJI_SNOWBOARDER);
+    a.vie=500;
+    a.esquive=0.30;
+    a.gain=30;
+    return a;
+}
+
+Attaquant constructeur_LugisteBarjo(Attaquant a){ //attaquant lent et resistant
+    strcpy(a.emoji, EMOJI_LUGISTE);
+    a.vie=2000;
+    a.esquive=0;
+    a.gain=50;
+    return a;
+}
+
+int** chargement(const char* nom_fichier, int* ligne, int* colonne);
+void amelioration(Case carte[]);
+void afficher_carte(int **carte, int taillecarte);
+void creer_carte(int **carte, int taillecarte);
+void creer_chemin(int **carte, int taillecarte);
+void defaite();
+void deplacement_attaquant(int** carte, int i, int j);
+int generer_attaquant(Case carte[], Attaquant liste_attaquant[], int avancee_vague, int vagues[][], int numero_vague);
+oid lancerpartie ();
+void menuDemarrage();
+void placement_de_defenseur(int carte[][], int taillecarte);
+void sauvegarde(const char* nom_fichier, int** tab, int ligne, int colonne);
