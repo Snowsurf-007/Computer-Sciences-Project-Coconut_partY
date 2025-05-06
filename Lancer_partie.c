9f12307c3ce3fda6adb95943172a84338458108b
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-05-06 11:35:19 +0200

    Update Lancer_partie.c

--- Lancer_partie.c
+++ Lancer_partie.c
@@ -1,33 +1,32 @@
 void lancerpartie (){
-    srand(time(NULL));
-    int taillecarte; //Variable pour la taille de la carte
-	int colonneCouronne;	
-	
-    // On génère une taille de carte aléatoire entre 35 et 50
-    taillecarte=rand()%16+35;
-
-    int **carte=(int**)malloc(taillecarte*sizeof(int*)); //Allocation dynamique de la mémoire pour la carte
-    
-    creer_carte(carte, taillecarte);
-    colonneCouronne=creer_chemin(carte, taillecarte);
-    afficher_carte(carte, taillecarte);
-    placement_de_defenseur(Case carte[]);
-
+	srand(time(NULL));
+	int taillecarte; //Variable pour la taille de la carte
+	int colonneCouronne;
 	int flocons=120;
+	
+    	// On génère une taille de carte aléatoire entre 35 et 50
+    	taillecarte=rand()%16+35;
 
-    while(carte[taillecarte-1][colonneCouronne]==12){
-        sleep(1);
-        deplacement_attaquant();
-        generer_attaquant();
-        afficher_carte(carte, taillecarte);
-        attaque_defenseurs();
-        placement_de_defenseur(Case carte[], flocons);
-        amelioration(flocons);
-    }
+    	int **carte=(int**)malloc(taillecarte*sizeof(int*)); //Allocation dynamique de la mémoire pour la carte
     
-    //On libère la mémoire allouée pour la carte
-    for (int i=0; i<taillecarte; i++) {
-    free(carte[i]);
-    }
-    free(carte);
+    	creer_carte(carte, taillecarte);
+    	colonneCouronne=creer_chemin(carte, taillecarte);
+    	afficher_carte(carte, taillecarte);
+    	placement_de_defenseur(Case carte[]);
+
+    	while(carte[taillecarte-1][colonneCouronne]==12){
+    		sleep(1);
+        	deplacement_attaquant();
+        	generer_attaquant();
+        	afficher_carte(carte, taillecarte);
+        	attaque_defenseurs();
+        	placement_de_defenseur(Case carte[], flocons);
+        	amelioration(flocons);
+    	}
+    
+    	//On libère la mémoire allouée pour la carte
+    	for (int i=0; i<taillecarte; i++) {
+    		free(carte[i]);
+   	}
+    	free(carte);
 }
