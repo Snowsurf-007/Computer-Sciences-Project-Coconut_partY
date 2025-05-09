e3a5f315ea3afe11c7f7027ac8cd66ad5e726439
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-05-09 09:02:48 +0200

    Update Victoire.c

--- Victoire.c
+++ Victoire.c
@@ -1,1 +1,7 @@
-
+void victoire(int* score) {
+    printf("\n \t== Vous avez gagné ! ==\n");
+    printf("\n \tScore=%d\n", *score);
+    sleep(2);
+    printf("\nRetour au menu principal...\n");
+    sleep(2);
+}
