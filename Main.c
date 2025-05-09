a88ba481ca46dc58de08457c82b34dd6caa655e7
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-05-09 09:21:18 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,7 +1,7 @@
 int main() {
     int jeu_en_cours=1; // Variable pour contrôler la boucle principale
 
-    while (jeu_en_cours){
+    while (jeu_en_cours==1){
         int choix_menu=menuDemarrage(); // Affiche le menu principal et récupère le choix
 
         switch (choix_menu){
@@ -10,9 +10,10 @@ int main() {
                 break;
             case 2:
             	//reprendre la partie
+            	break;
             case 3:
                 printf("A plus 👋😊\n");
-                jeu_en_cours = 0; // Quitte la boucle principale
+                jeu_en_cours=0; // Quitte la boucle principale
                 break;
             default:
                 printf("Choix invalide. Veuillez réessayer.\n");
