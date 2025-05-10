7ff04dd8e30cefbe27679728c697b52343859b6d
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-05-10 16:45:12 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,16 +1,29 @@
 int main() {
+    srand(time(NULL));
     int jeu_en_cours=1; // Variable pour contrôler la boucle principale
+    // Initialisation des variables
+    Case** carte=NULL; 
+    int taillecarte=0;
+    Defenseur* defenseurs=NULL;
+    int nbDefenseurs=0;
+    EnnemiActif* ennemis=NULL;
+    int nbEnnemis=0;
+    int score=0;
+    int flocons=120;
+    int vague=0;
+
 
     while (jeu_en_cours==1){
         int choix_menu=menuDemarrage(); // Affiche le menu principal et récupère le choix
 
         switch (choix_menu){
             case 1:
-                lancerpartie(); // Lance une nouvelle partie
+                lancerpartie(&carte, &taillecarte, &defenseurs, &nbDefenseurs, &ennemis, &nbEnnemis, &score, &flocons, &vague); // Lance une nouvelle partie
                 break;
             case 2:
-            	//reprendre la partie
-            	break;
+                chargement("sauvegarde.txt", &carte, &taillecarte, &defenseurs, &nbDefenseurs, &ennemis, &nbEnnemis, &score, &flocons, &vague);
+                lancerpartie(&carte, &taillecarte, &defenseurs, &nbDefenseurs, &ennemis, &nbEnnemis, &score, &flocons, &vague); // Reprend la partie chargée
+            break;
             case 3:
                 printf("A plus 👋😊\n");
                 jeu_en_cours=0; // Quitte la boucle principale
