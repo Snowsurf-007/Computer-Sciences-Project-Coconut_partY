5f22b9784f1a1e76f8c3f6cd12d31294032741c7
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-05-10 16:38:27 +0200

    Update Creation_de_la_carte.c

--- Creation_de_la_carte.c
+++ Creation_de_la_carte.c
@@ -1,130 +1,67 @@
-void afficher_carte(Case** carte, int taillecarte){
-    
-    // On affiche la carte
+void afficher_carte(Case** carte, int taillecarte) {
     printf("    ");
-    for (int i=0; i<taillecarte; i++){
-    	if(i<26){
-        	printf("%c ", 'a'+i);// minuscules jusqu'à 26
-    	}
-    	else{
-    		printf("%c ", 'A'+(i-26));// majuscules après
-    	}
+    for (int i = 0; i < taillecarte; i++) {
+        if (i < 26) {
+            printf("%c ", 'a' + i);
+        } else {
+            printf("%c ", 'A' + (i - 26));
+        }
     }
-    printf("\n");
-    printf("    ");
-    for (int i=0; i<taillecarte; i++){
+    printf("\n    ");
+    for (int i = 0; i < taillecarte; i++) {
         printf("__");
     }
     printf("\n");
-    
-    for (int i=0; i<taillecarte; i++) {
-    	printf("%02d |", i+1);  // Numéro de ligne
-    	for (int j=0; j<taillecarte; j++) {
-    		switch (carte[i][j].type){
-        		case 0:
-        		case 1:
-        		case 2:
-        		case 3:
-           			printf("%s ", EMOJI_NEIGE);
-            			break;
-        		case 4:
-            			printf("%s", EMOJI_PIERRE);
-            			break;
-        		case 5:
-            			printf("%s", EMOJI_SAPIN);
-           			break;
-        		case 6:
-         			printf("%s", EMOJI_DRAPEAU);
-            			break;
-        		case 7:
-            			printf("%s", EMOJI_COURONNE);
-            			break;
-            		case 8:
-            			printf("%s ", EMOJI_SKIEUR);
-            			break;
-            		case 9:
-            			printf("%s", EMOJI_SNOWBOARDER);
-            			break;
-            		case 10:
-            			printf("%s", EMOJI_LUGISTE);
-            			break;
-			case 11:
-            			printf("%s", EMOJI_PINGOUIN);
-            			break;
-			case 12:
-            			printf("%s", EMOJI_BONHOMMENEIGE);
-            			break;
-			case 13:
-            			printf("%s", EMOJI_OURS);
-            			break;
-        		default:
-            			exit(5);
-    		}
-    	}
-    	printf("|\n");
-    	}
-    	printf("    ");
-    	for (int i=0; i<taillecarte; i++){
-    	    printf("‾‾");
-    	}
-    	printf("\n");
-}
 
-void creer_carte(Case** carte, int taillecarte) {
-    for (int i=0; i<taillecarte; i++){
-        carte[i]=(Case*)malloc(taillecarte*sizeof(Case));
-        if (carte[i]==NULL) {
-            printf("Erreur d'allocation mémoire\n");
-            exit(1); //Terminer le programme si l'allocation échoue
+    for (int i = 0; i < taillecarte; i++) {
+        printf("%02d |", i + 1);
+        for (int j = 0; j < taillecarte; j++) {
+            switch (carte[i][j].type) {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    printf("%s ", EMOJI_NEIGE);
+                    break;
+                case 4:
+                    printf("%s", EMOJI_PIERRE);
+                    break;
+                case 5:
+                    printf("%s", EMOJI_SAPIN);
+                    break;
+                case 6:
+                    printf("%s", EMOJI_DRAPEAU);
+                    break;
+                case 7:
+                    printf("%s", EMOJI_COURONNE);
+                    break;
+                case 8:
+                    printf("%s ", EMOJI_SKIEUR);
+                    break;
+                case 9:
+                    printf("%s", EMOJI_SNOWBOARDER);
+                    break;
+                case 10:
+                    printf("%s", EMOJI_LUGISTE);
+                    break;
+                case 11:
+                    printf("%s", EMOJI_PINGOUIN);
+                    break;
+                case 12:
+                    printf("%s", EMOJI_BONHOMMENEIGE);
+                    break;
+                case 13:
+                    printf("%s", EMOJI_OURS);
+                    break;
+                default:
+                    exit(5);
+            }
         }
-        for (int j=0; j<taillecarte; j++){
-            carte[i][j].type=rand()%6; //Remplir la carte avec des valeurs aléatoires
-        }
+        printf("|\n");
     }
-}
-
-void creer_chemin(Case** carte, int taillecarte){
-    int j=rand()%(taillecarte-6)+3; //Colonne de départ
-    int direction, directionprecedente;
-    
-    // Premier mouvement, on va tout droit (vers le bas)
-    direction=0;
-    carte[0][j].type=6; // Placement du drapeau sur la premiere case
-    carte[1][j].type=6; // Placement du drapeau sur la deuxieme case
-    
-    directionprecedente=direction;
-
-    // Créer le chemin en suivant la logique aléatoire mais en garantissant le premier et le dernier mouvement vers le bas
-    for (int i=2; i<taillecarte-1; i++) {
-        //Déplacement latéral aléatoire (mais pas l'opposé de la direction precedente) : -1 (gauche), 0 (tout droit), 1 (droite)
-        do{
-        	direction=rand()%3-1;
-        }while (directionprecedente==-direction);
-        
-        directionprecedente=direction;
-
-        //Calcul de la nouvelle position (temporaire pour vérification)
-        int nouvellecolonne=j+direction;
-
-        //Vérification des bornes
-        if (nouvellecolonne<0){
-            nouvellecolonne=0;
-        }
-        if (nouvellecolonne>=taillecarte){
-            nouvellecolonne=taillecarte-1;
-        }
-
-        //Si on change de colonne, on met un drapeau au dessus de la nouvelle case
-        if (direction!=0 && i>0) {
-            carte[i-1][nouvellecolonne].type=6;
-        }
-
-        //Mise à jour de la position j
-        j=nouvellecolonne;
-
-        //On place le drapeau sur la case actuelle
-        carte[i][j].type=6;
+    printf("    ");
+    for (int i = 0; i < taillecarte; i++) {
+        printf("‾‾");
     }
-    //On place la couronne sur la derniere case en allant tout droit (vers le bas)
-	carte[taillecarte-1][j].type=7;
+    printf("\n");
 }
