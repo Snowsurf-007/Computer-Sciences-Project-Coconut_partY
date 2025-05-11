5a3e288f8aa4ab3d76e3519683808993ab730cd4
M4x0u77 <max19.monteil@gmail.com>
2025-05-11 15:16:18 +0200

    Delete victoire.c

--- victoire.c
+++
@@ -1,7 +0,0 @@
-void victoire(int* score){
-    printf("\n \t== Vous avez gagné ! ==\n");
-    printf("\n \tScore=%d\n", *score);
-    sleep(2);
-    printf("\nRetour au menu principal...\n");
-    sleep(2);
-}
