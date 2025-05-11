1e4c531584c9a2a4b4573dc1cd5889a4f0423e39
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-05-11 16:11:33 +0200

    Delete interface_utilisateur.h

--- interface_utilisateur.h
+++
@@ -1,7 +0,0 @@
-#include "fonctions.h"
-void afficher_carte(Case** carte, int taillecarte);
-void placement_de_defenseur(Case** carte, int taillecarte, int* flocons, Defenseur* liste_defenseur, int* nb_defenseur);
-void lancerpartie(Case*** carte, int* taillecarte, Defenseur** defenseurs, int* nbDefenseurs, EnnemiActif** ennemis, int* nbEnnemis, int* score, int* flocons, int* vague);
-int menuDemarrage();
-void defaite(int* score);
-void victoire(int* score);
