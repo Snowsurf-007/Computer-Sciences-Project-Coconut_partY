9ca043c7bb244b194dc623ccd0102f2cc9260a08
M4x0u77 <max19.monteil@gmail.com>
2025-05-11 15:15:21 +0200

    Delete defaite.c

--- defaite.c
+++
@@ -1,7 +0,0 @@
-void defaite(int* score){
-    printf("\n \t== Vous avez perdu ! ==\n");
-    printf("\n \tScore=%d\n", *score);
-    sleep(2);
-    printf("\nRetour au menu principal...\n");
-    sleep(2);
-}
